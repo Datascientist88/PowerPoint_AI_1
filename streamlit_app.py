024f4484a7eee6f1a7832d5df135378a96540d3a
Github@git.com <119727641+Datascientist88@users.noreply.github.com>
2024-01-31 17:56:23 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 import streamlit.components.v1 as components
 st.title('🎈 Presentation Deck')
-components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vReFZMUPpDC1hGJR-KPJSRXGY8B9_DXwIZ0Ky0G99Ch_cq4m0-o8v9ONUDStr9T-mEVbs80kfPdKqZV/embed?start=false&loop=false&delayms=3000" ,height=650)
+components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vReFZMUPpDC1hGJR-KPJSRXGY8B9_DXwIZ0Ky0G99Ch_cq4m0-o8v9ONUDStr9T-mEVbs80kfPdKqZV/embed?start=false&loop=false&delayms=3000" ,height=850)
